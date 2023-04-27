314bb07abf47c76cac191c68ec4021aa444f8ee6
SDBoahen <SDBoahen@gmail.com>
2023-04-27 09:55:32 -0400

    A Little Activity for some Github Practice :)

---
+++ README.md
@@ -0,0 +1,5 @@
+# Change This - Make this About ✨YOU✨
+
+## There's an index.js file in this code base. You have two goals:
+- Alter the `meObj` so it matches relevant information about ✨YOU✨
+- Complete the logic in the `introduceMe` function to print out the 2 statments that the comments are guiding you to print out.
---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hey you! Yeah you! Awesome Developer! You're doing Great Fam! 💪🧑‍💻🙌")
+
+
+
+const meObj = {
+    name: "Sam",
+    hometown: "The Boogie-Down - BX 🙅🏾‍♂️",
+    favoriteColors: ["gold", "navy blue", "forest green", "red"]
+}
+
+
+function introduceMe (personObj){
+    // print out: "Hi, my name is {your name}, and I'm from {your hometown}"
+
+    // print out: "My favorite colors are {your favorite colors}"
+}
